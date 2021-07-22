6db69bf01c0439c4779907c9dce4e6fe9ae6ebe8
DoctorGurke <docgurk@gmail.com>
2021-07-23 00:32:08 +0200

    moved project to wpf
    basic debug richtextbox for debug print

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AddonCleaner.Type;
+using System.IO;
+
+namespace AddonCleaner {
+    public partial class MainWindow : Window {
+		private RichTextBox Output;
+
+        public MainWindow() {
+            InitializeComponent();
+			Output = (RichTextBox)this.FindName("debugoutput");
+
+			DirectoryInfo addonDir = new("C:\\Users\\docgu\\Desktop\\addon");
+			var rootNode = new DirectoryNode(addonDir);
+
+			//rootNode.PrintTree();
+			//System.Console.WriteLine(rootNode.self.info.FullName);
+			foreach(var path in rootNode.GetFilesRecursively()) {
+				PrintToConsole(path);
+			}
+		}
+
+		public void PrintToConsole(string text) {
+			Application.Current.Dispatcher.Invoke(() => {
+				Output.Document.Blocks.Add(new Paragraph(new Run(text)));
+				Output.ScrollToEnd();
+			});
+		}
+	}
+}
