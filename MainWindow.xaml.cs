72273b53b3df3164e918af3b1708465b5aa4cd8f
DoctorGurke <docgurk@gmail.com>
2021-07-27 00:48:34 +0200

    added addon name field and cleaned up code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,60 +13,6 @@ using System.IO.Compression;
 
 namespace AddonCleaner {
 
-	public partial class SelectionNode : CheckBox {
-		public bool IsDirectory {
-			get { return (bool)GetValue(IsDirectoryProperty); }
-			set { SetValue(IsDirectoryProperty, value); }
-		}
-
-		private SelectionItem selectionItem;
-
-		public SelectionNode(SelectionItem selectionItem) {
-			this.selectionItem = selectionItem;
-			this.IsChecked = this.selectionItem.enabled;
-			if(this.selectionItem is SelectionFile file) {
-				this.Content = "File | " + file.info.Name;
-			} else if(this.selectionItem is SelectionDirectory dir) {
-				this.Content = "Dir | " + dir.info.Name;
-			}
-			this.selectionItem.OnEnabledChanged += HandleCheckedChanged;
-
-			// we don't care about empty folders, do we? 
-			if(this.selectionItem.node.CheckEmpty()) {
-				this.IsEnabled = false;
-			}
-			
-			this.Click += new RoutedEventHandler(selection_node_checked);
-		}
-
-		private void HandleCheckedChanged(bool state) {
-			//MainWindow.PrintToConsole($"{state}");
-			IsChecked = state;
-		}
-
-		private static void selection_node_checked(object sender, RoutedEventArgs e) {
-			if(sender is SelectionNode selection) {
-				if(selection.selectionItem is SelectionDirectory dir) {
-					switch(selection.IsChecked) {
-						case true: dir.node.EnableRecursively(); break;
-						case false: dir.node.DisableRecursively(); break;
-					}
-				} else if(selection.selectionItem is SelectionFile file) {
-					switch(selection.IsChecked) {
-						case true: file.Enable(); break;
-						case false: file.Disable(); break;
-					}
-				}
-				// make sure ascending dir gets enabled/disabled accordingly
-				// example: all files are disabled = disable folder
-				//			folder is disabled but file is enabled = enable folders
-				selection.selectionItem.node.VerifyContents(true);
-			}
-		}
-
-		public static readonly DependencyProperty IsDirectoryProperty = DependencyProperty.Register("Directory", typeof(bool), typeof(SelectionNode));
-	}
-
 	public partial class MainWindow : Window {
 		private static RichTextBox Output;
 		private static StackPanel MainPanel;
@@ -105,34 +51,45 @@ namespace AddonCleaner {
 			});
 		}
 
-		private void PackAddon(object sender, RoutedEventArgs e) {
-			var inputBox = (System.Windows.Controls.RichTextBox)this.FindName("InputLocation");
-			string inputLocation = new TextRange(inputBox.Document.ContentStart, inputBox.Document.ContentEnd).Text.Replace("\r\n", "");
-			// simple validate to check if the path is empty
-			if(inputLocation.Replace(" ", "") == "") { PrintToConsole("Invalid input directory"); return; }
-			//PrintToConsole($"input {inputLocation}");
+		private void ValidateInputBox(string inputName, out string text) {
+			var inputBox = (System.Windows.Controls.RichTextBox)this.FindName(inputName);
+			string inputText = new TextRange(inputBox.Document.ContentStart, inputBox.Document.ContentEnd).Text.Replace("\r\n", "");
+			text = inputText;
+		}
 
-			var outputBox = (System.Windows.Controls.RichTextBox)this.FindName("OutputLocation");
-			string outputLocation = new TextRange(outputBox.Document.ContentStart, outputBox.Document.ContentEnd).Text.Replace("\r\n", "");
-			// simple validate to check if the path is empty
-			if(outputLocation.Replace(" ", "") == "") { PrintToConsole("Invalid output directory"); return; }
-			//PrintToConsole($"output {outputLocation}");
+		private void PackAddon(object sender, RoutedEventArgs e) {
+
+			string addonName;
+			ValidateInputBox("AddonNameInput", out addonName);
+			if(addonName == null) { PrintToConsole("Invalid addon name"); return; }
+
+			// technically not used since the activeRootNode gets set as soon as you select a directory
+			string inputPath;
+			ValidateInputBox("InputLocation", out inputPath);
+			if(addonName == null) { PrintToConsole("Invalid input directory"); return; }
+
+			string outputPath;
+			ValidateInputBox("OutputLocation", out outputPath);
+			if(addonName == null) { PrintToConsole("Invalid output directory"); return; }
 
 			var rootParentDir = activeRootNode.self.info.Parent.FullName;
 
-			// some fool proof last minute checks for directories etc
-			if(!Directory.Exists(rootParentDir))
-				return;
+			// create temp dir
+			Directory.CreateDirectory($"{outputPath}\\addon-release-temp");
 
-			Directory.CreateDirectory($"{outputLocation}\\addon-release-temp");
+			//copy enabled files/dirs recursively
+			CopyEnabledFiles(activeRootNode, rootParentDir, $"{outputPath}\\addon-release-temp");
 
-			CopyEnabledFiles(activeRootNode, rootParentDir, $"{outputLocation}\\addon-release-temp");
 			try {
-				ZipFile.CreateFromDirectory($"{outputLocation}\\addon-release-temp\\{activeRootNode.self.info.Name}", $"{outputLocation}\\addon-release.zip");
+				// zip up the addon with the addon name
+				ZipFile.CreateFromDirectory($"{outputPath}\\addon-release-temp\\{activeRootNode.self.info.Name}", $"{outputPath}\\{addonName}.zip");
+				PrintToConsole("Pack complete!");
 			} catch {
 				PrintToConsole("Packing addon failed! A zip with that name possibly already exists!");
 			}
-			Directory.Delete($"{outputLocation}\\addon-release-temp", true);
+
+			// delete temp dir
+			Directory.Delete($"{outputPath}\\addon-release-temp", true);
 		}
 
 		private void CopyEnabledFiles(DirectoryNode node, string rootParentDir, string outputLocation) {
@@ -183,5 +140,58 @@ namespace AddonCleaner {
 				}
 			}
 		}
+	}
+
+	public partial class SelectionNode : CheckBox {
+		public bool IsDirectory {
+			get { return (bool)GetValue(IsDirectoryProperty); }
+			set { SetValue(IsDirectoryProperty, value); }
+		}
+
+		private SelectionItem selectionItem;
+
+		public SelectionNode(SelectionItem selectionItem) {
+			this.selectionItem = selectionItem;
+			this.IsChecked = this.selectionItem.enabled;
+			if(this.selectionItem is SelectionFile file) {
+				this.Content = "File | " + file.info.Name;
+			} else if(this.selectionItem is SelectionDirectory dir) {
+				this.Content = "Dir | " + dir.info.Name;
+			}
+			this.selectionItem.OnEnabledChanged += HandleCheckedChanged;
+
+			// we don't care about empty folders, do we? disable the button entirely
+			if(this.selectionItem.node.CheckEmpty()) {
+				this.IsEnabled = false;
+			}
+
+			this.Click += new RoutedEventHandler(selection_node_checked);
+		}
+
+		private void HandleCheckedChanged(bool state) {
+			IsChecked = state;
+		}
+
+		private static void selection_node_checked(object sender, RoutedEventArgs e) {
+			if(sender is SelectionNode selection) {
+				if(selection.selectionItem is SelectionDirectory dir) {
+					switch(selection.IsChecked) {
+						case true: dir.node.EnableRecursively(); break;
+						case false: dir.node.DisableRecursively(); break;
+					}
+				} else if(selection.selectionItem is SelectionFile file) {
+					switch(selection.IsChecked) {
+						case true: file.Enable(); break;
+						case false: file.Disable(); break;
+					}
+				}
+				// make sure ascending dir gets enabled/disabled accordingly
+				// example: all files are disabled = disable folder
+				//			folder is disabled but file is enabled = enable folders
+				selection.selectionItem.node.VerifyContents(true);
+			}
+		}
+
+		public static readonly DependencyProperty IsDirectoryProperty = DependencyProperty.Register("Directory", typeof(bool), typeof(SelectionNode));
 	}
 }
